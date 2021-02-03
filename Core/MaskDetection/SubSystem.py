c427fcd59ac79f3b8936d0a8aa15aacd1f459f97
blam19 <blam19@calpoly.edu>
2021-02-02 18:04:35 -0800

    stubbing done

--- Core/MaskDetection/SubSystem.py
+++ Core/MaskDetection/SubSystem.py
@@ -1,26 +1,29 @@
-class SubSystem:
-    # inputs:    preprocessed image data: unknown atm
-    # outputs:   mask detection status: POSSIBLY FLAG?
-    #           image data: unknown atm
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+import argparse
+import cv2
+import os
 
-    def __init__(self, face):
+class SubSystem:
+    # inputs:    preprocessed image data: image frame
+    # outputs:   mask detection status: flag
+    #            image data: xy coordinates of mask
+    def __init__(self, face, maskStatus):
         self.face = face
+        self.maskStatus = maskStatus
 
     def initialize(self):
         print("Hello this is the MaskDetection subsystem")
 
-    # prediction method (adrian rosebrock)
-    def prediction(self):
-        pass
+    def prediction(self, maskModel):
+        self.maskStatus = maskModel.predict(self.face)[0]
 
-#        pass the face through the model to determine if the face
-# 		# has a mask or not
-# 		(mask, withoutMask) = model.predict(face)[0]
-#
-# 		# determine the class label and color we'll use to draw
-# 		# the bounding box and text
-# 		label = "Mask" if mask > withoutMask else "No Mask"
-# 		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-#
-# 		# include the probability in the label
-# 		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+    #       pass the face through the model to determine if the face
+    # 		has a mask or not
+    # 		(mask, withoutMask) = model.predict(face)[0]
+
+    # i dont think need this
+    def statusUpdate(self):
